a09c6bf1ef813007b1632075ad81763e569e904a
Lo_op <shaikhfarhaan312@gmail.com>
2025-02-08 16:22:48 -0500

    added android compile options

--- keyhandler/build.gradle.kts
+++ keyhandler/build.gradle.kts
@@ -137,6 +137,10 @@ android {
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
 }
 
 compose.desktop {

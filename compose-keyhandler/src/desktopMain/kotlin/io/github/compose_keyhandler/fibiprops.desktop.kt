7468c4dffc1c4db9b0a424fd94cb0621d7cb130a
Lo_op <shaikhfarhaan312@gmail.com>
2025-07-16 17:41:25 -0400

    remove useless files

--- compose-keyhandler/src/desktopMain/kotlin/io/github/compose_keyhandler/fibiprops.desktop.kt
+++
@@ -1,4 +0,0 @@
-package io.github.compose_keyhandler
-
-actual val firstElement: Int = 1
-actual val secondElement: Int = 2
--- compose-keyhandler/src/iosMain/kotlin/io/github/compose_keyhandler/fibiprops.ios.kt
+++
@@ -1,4 +0,0 @@
-package io.github.compose_keyhandler
-
-actual val firstElement: Int = 3
-actual val secondElement: Int = 4
